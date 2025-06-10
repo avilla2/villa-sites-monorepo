c22f3c05e3a3c19dbecd4d40b2a492fbb0cf084d
avilla2 <a.villa12@outlook.com>
2025-06-09 23:18:17 -0700

    chore: updates

--- websites/pacwest/moduleFederation.config.js
+++ websites/pacwest/moduleFederation.config.js
@@ -1,11 +1,11 @@
-const { dependencies } = require('../../package.json');
+const { dependencies } = require('../../package.json')
 
 module.exports = {
   name: 'villa_web_solutions',
   remotes: {
-    villa_components: process.env.NODE_ENV === 'production' ? 
-        'villa_components@https://dev.villawebsolutions.com/remoteEntry.js' : 
-        'villa_components@http://localhost:3004/remoteEntry.js'
+    villa_components: process.env.NODE_ENV === 'production'
+      ? 'villa_components@https://dev.villawebsolutions.com/remoteEntry.js'
+      : 'villa_components@http://localhost:3004/remoteEntry.js'
   },
   shared: {
     ...dependencies,
@@ -13,19 +13,19 @@ module.exports = {
       singleton: true,
       import: 'react',
       shareScope: 'default',
-      requiredVersion: dependencies.react,
+      requiredVersion: dependencies.react
     },
     'react-dom': {
       singleton: true,
-      requiredVersion: dependencies['react-dom'],
+      requiredVersion: dependencies['react-dom']
     },
     '@mui/material': {
-        singleton: true,
-        requiredVersion: dependencies['@mui/material']
+      singleton: true,
+      requiredVersion: dependencies['@mui/material']
     },
     '@mui/icons-material': {
-    singleton: true,
-    requiredVersion: dependencies['@mui/icons-material']
-    },
-  },
-};
+      singleton: true,
+      requiredVersion: dependencies['@mui/icons-material']
+    }
+  }
+}
